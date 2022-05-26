1103e49369777bc1eafc797e5a57dc6d02c28dae
Varun Mishra <varun@Varuns-MacBook-Pro-2.local>
2022-05-26 19:29:52 +0530

    Initial Commit

--- variables.sh
+++ variables.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-CLUSTER_NAME='priv-fra-ssm-eks-cluster-01'
-REGION=eu-central-1
+CLUSTER_NAME='eks-piiano-sandbox-04'
+REGION=us-east-2
 HTTP_PROXY_ENDPOINT_SERVICE_NAME="" # leave blank for no proxy, or populate with a VPC endpoint ID to create a PrivateLink powered connection to a proxy server
 KEY_PAIR=""
-VERSION='1.16' # K8s version to deploy
-AMI_ID=ami-0bf7306240d09dcdd # AWS managed AMI for EKS worker nodes
-INSTANCE_TYPE=t3.large # instance type for EKS worker nodes
-S3_STAGING_LOCATION=jasbarto-eks-frankfurt-cfn # S3 location to be created to store Cloudformation templates and a copy of the kubectl binary
+VERSION='1.22' # K8s version to deploy
+AMI_ID=ami-0ce29f1698cab2968 # AWS managed AMI for EKS worker nodes
+INSTANCE_TYPE=t3.medium # instance type for EKS worker nodes
+S3_STAGING_LOCATION=s3-piiano-eks-shared-resources-us-east-2 # S3 location to be created to store Cloudformation templates and a copy of the kubectl binary
 ENABLE_PUBLIC_ACCESS=false
 ENABLE_FARGATE=false
 FARGATE_PROFILE_NAME=PrivateFargateProfile
